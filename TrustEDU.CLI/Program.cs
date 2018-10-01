95f2ebf47b27aea4723035ccee687dfdc3b76537
Tommy <tommy.fcode@gmail.com>
2018-10-01 12:19:06 +0700

    Initialize the coding structrue

---
+++ TrustEDU.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrustEDU.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TrustEDU Network Command Line Interface!");
+        }
+    }
+}
